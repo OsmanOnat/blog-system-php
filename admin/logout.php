bd7964cd2e1198d6d7183b78e2ceaf2890573b76
Osman Onat <84340387+OsmanOnat@users.noreply.github.com>
2022-01-30 20:03:34 +0300

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -2,6 +2,10 @@
 session_start();
 require_once("php/connection.php");
 
+if (empty(isset($_SESSION['session']))) { // oturum açılmadığı için false döner ve bu sayfa açılmaz!
+    header("Location:loginpage.php");
+}
+
 $changeAdminStatus = $connection->query('UPDATE admin SET admin_status = 0 WHERE admin_kullanici_adi = "' . $_SESSION['adminUsernameStatus'] . '" ');
 
 if ($changeAdminStatus == TRUE) {
